17ef25c87961dd7eac14a9855bc36cc402a3a64e
iGuest <nasichj@uw.edu>
2014-11-03 11:25:35 -0800

    Initial changes

--- js/app.js
+++ js/app.js
@@ -4,4 +4,11 @@
  */
 
 "use strict";
-
+//the first thing we pass is the file we work on and the second is elements we need/
+// no semicolon at the end of declaration sothat the controller isobvious
+//$scope intialization functin- angular s responsible for creating it
+//to initialize controllr
+angular.module('MovieApp', [])
+    .controller('MoviesController', function($scope){
+        $scope.movies = movies;
+    });
